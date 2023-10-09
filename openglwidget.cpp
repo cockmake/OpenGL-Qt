799f521f9ecca1e818f6ae555e08445ec3c6207c
cockmake <2359016829@qq.com>
2023-10-09 23:39:35 +0800

    created project

---
+++ openglwidget.cpp
@@ -0,0 +1,26 @@
+﻿#include "openglwidget.h"
+
+OpenGLWidget::OpenGLWidget(QWidget *parent): QOpenGLWidget(parent)
+{
+    cout<<"运行开始"<<endl;
+}
+
+void OpenGLWidget::initializeGL()
+{
+
+    initializeOpenGLFunctions();
+}
+
+void OpenGLWidget::resizeGL(int w, int h)
+{
+    cout<<w<<' '<<h<<endl;
+}
+
+void OpenGLWidget::paintGL()
+{
+    // 窗口大小发生变化时会触发一次绘制
+    // 函数使用之前必须要进行初始化，即指向显卡中的函数，否则函数为空指针
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    cout<<"运行结束"<<endl;
+}
---
+++ openglwidget.h
@@ -0,0 +1,26 @@
+﻿#ifndef OPENGLWIDGET_H
+#define OPENGLWIDGET_H
+
+#include <QObject>
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_5_Core>
+#include <iostream>
+using namespace std;
+class OpenGLWidget : public QOpenGLWidget, QOpenGLFunctions_4_5_Core
+{
+    Q_OBJECT
+public:
+    explicit OpenGLWidget(QWidget *parent = nullptr);
+
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
+signals:
+
+};
+
+#endif // OPENGLWIDGET_H
