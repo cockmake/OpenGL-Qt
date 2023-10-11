0c46c4fb0b072dc2cda19156d81bc80c954751dd
cockmake <2359016829@qq.com>
2023-10-11 23:09:25 +0800

    i am going to complete the pratice for chapter one

---
+++ Chapter-One/practice/widget.cpp
@@ -0,0 +1,11 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :QWidget(parent) ,ui(new Ui::Widget){
+    ui->setupUi(this);
+//    ui->openGLWidget->setGeometry(this->geometry());
+
+}
+Widget::~Widget(){
+    delete ui;
+}
